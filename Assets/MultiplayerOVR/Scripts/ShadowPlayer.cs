f2972bcb82ad50b3ce3450e2d02eb74ceade5dc4
victor <vgarzom@gmail.com>
2019-10-24 10:52:36 -0500

    adds a shadow of the player to the environment

---
+++ Assets/MultiplayerOVR/Scripts/ShadowPlayer.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+public class ShadowPlayer : MonoBehaviourPun, IPunObservable
+{
+    [SerializeField]
+    private Vector3 initialPosition;
+
+    private Vector3 currentPosition;
+
+    [SerializeField]
+    private string id;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        initialPosition = Vector3.zero;
+        currentPosition = new Vector3(initialPosition.x, initialPosition.y, initialPosition.z);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void AddPosition(Vector3 deltaPosition) {
+        currentPosition.x += deltaPosition.x;
+        currentPosition.y += deltaPosition.y;
+        currentPosition.z += deltaPosition.z;
+
+
+    }
+
+    public void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
+    {
+        if (stream.IsWriting)
+        {
+            stream.SendNext(this.id);
+        }
+
+        else
+        {
+            this.id = (string)stream.ReceiveNext();
+        }
+    }
+
+    public void SetId(string _id) {
+        this.id = _id;
+    }
+}

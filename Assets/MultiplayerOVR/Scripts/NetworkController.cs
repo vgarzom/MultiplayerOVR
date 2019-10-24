6a47fcab033411e8b42547c11e95a8c3a490aff6
victor <vgarzom@gmail.com>
2019-10-24 09:40:54 -0500

    first commit

---
+++ Assets/MultiplayerOVR/Scripts/NetworkController.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using Photon.Pun;
+using Photon.Realtime;
+
+public class NetworkController : MonoBehaviourPunCallbacks
+{
+    string _room = "multiplayerOVR";
+
+    [Tooltip("The maximum number of players per room. When a room is full, it can't be joined by new players, and so new room will be created")]
+    [SerializeField]
+    private byte maxPlayersPerRoom = 4;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (PhotonNetwork.IsConnected) {
+            JoinRoom();
+        }
+        else
+        {
+            Debug.Log("Trying to connect to photon server...");
+            PhotonNetwork.GameVersion = "1.0";
+            PhotonNetwork.ConnectUsingSettings();
+        }
+
+    }
+
+    void JoinRoom() {
+        RoomOptions roomOptions = new RoomOptions() { MaxPlayers = maxPlayersPerRoom };
+        PhotonNetwork.JoinOrCreateRoom(_room, roomOptions, TypedLobby.Default);
+    }
+
+    public override void OnConnectedToMaster()
+    {
+        Debug.Log("Just connected to master!");
+        JoinRoom();
+    }
+
+    override public void OnJoinedLobby()
+    {
+        Debug.Log("Just Joined to Lobby!");
+    }
+
+    override public void OnJoinedRoom()
+    {
+        PhotonNetwork.Instantiate("NetworkedPlayer", Vector3.zero, Quaternion.identity, 0);
+        Debug.Log("Joined to room");
+    }
+}
---
+++ Assets/MultiplayerOVR/Scripts/NetworkedPlayer.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+public class NetworkedPlayer : MonoBehaviourPun, IPunObservable
+{
+    [SerializeField]
+    private GameObject avatar;
+
+    [SerializeField]
+    private Transform playerGlobal;
+
+    [SerializeField]
+    private Transform playerLocal;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Networked player has been instantiated!");
+
+        if (photonView.IsMine)
+        {
+            playerGlobal = GameObject.Find("OVRPlayerController").transform;
+            playerLocal = playerGlobal.Find("OVRCameraRig/TrackingSpace/CenterEyeAnchor");
+
+            transform.SetParent(playerLocal);
+            transform.localPosition = new Vector3(0, 0.5f, 0);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
+    {
+        if(stream.IsWriting) {
+            stream.SendNext(playerGlobal.position);
+            stream.SendNext(playerGlobal.rotation);
+            stream.SendNext(playerLocal.localPosition);
+            stream.SendNext(playerLocal.localRotation);
+        }
+
+        else {
+            this.transform.position = (Vector3)stream.ReceiveNext();
+            this.transform.rotation = (Quaternion)stream.ReceiveNext();
+            avatar.transform.localPosition = (Vector3)stream.ReceiveNext();
+            avatar.transform.localRotation = (Quaternion)stream.ReceiveNext();
+        }
+    }
+}
